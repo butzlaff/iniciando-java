e234032a31c6f54f861513b7c590f160dd279b3c
butzlaff <emiliobutz@gmail.com>
2023-10-01 16:42:09 -0300

    feat:iniciando em java

---
+++ src/MyClass.java
@@ -0,0 +1,13 @@
+public class MyClass {
+  public static void main(String[] args) {
+    System.out.print("Olá, seja bem vindo!");
+  }
+
+  public static void MyMethod() {
+    System.out.print("Este é meu primeiro metodo em java");
+  }
+
+  String[] cars = {"Volvo", "BMW", "Ford", "Mazda"};
+
+
+}
